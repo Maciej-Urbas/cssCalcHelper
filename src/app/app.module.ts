3f999c99283638add1187e8905cc6cfdf0e1cac9
Maciej Urbas <maciej.urbas3@gmail.com>
2023-10-22 19:25:28 +0200

    :heavy_plus_sign: add @angular/material for `cdk`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ClipboardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
