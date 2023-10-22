538921cd9f7a4b37b54905f41abb2048bfe10e0d
Maciej Urbas <maciej.urbas3@gmail.com>
2023-10-22 19:28:26 +0200

    :sparkles: Set Copy to Clipboard functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { Clipboard } from '@angular/cdk/clipboard';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +14,14 @@ export class AppComponent implements OnInit {
   title = 'cssCalcHelper';
 
   // FOR 20.10.2023 A.D.
-  // npm install @angular/cdk
+  // npm install @angular/material
+
+  constructor(private clipboard: Clipboard) {}
 
   // Declare variable for pxValue
   numberOfPxValue = '';
   clampForPhones = '';
+  clampForPcs = '';
 
   ngOnInit(): void {
     // Set `numberOfPxValue` with data from localStorage
@@ -32,20 +35,57 @@ export class AppComponent implements OnInit {
     localStorage.setItem('numberOfPxStorageValue', e.target.value);
   }
 
-  copyMessage(val: string) {
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
-    selBox.value = val;
-    document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    document.execCommand('copy');
-    document.body.removeChild(selBox);
+  copyMessageForPhonesResolution() {
+    // Download value from local storage and parse it to float
+    const downloadValue = parseFloat(
+      localStorage.getItem('numberOfPxStorageValue')!
+    );
+
+    // Set middleValue for calculation
+    let middleValue = downloadValue / 360;
+
+    // Calculate min and max value
+    let minValue = middleValue * 210;
+    let maxValue = middleValue * 776;
+
+    // End calculation for middleValue
+    middleValue = middleValue * 100;
+
+    // Create CSS `clamp()` for calculations
+    this.clampForPhones = `clamp(min(${minValue.toFixed(
+      2
+    )}px), ${middleValue.toFixed(2)}vw, max(${maxValue.toFixed(2)}px));`;
+
+    console.log(this.clampForPhones);
+
+    // Return copied phrase for CSS
+    return this.clipboard.copy(this.clampForPhones);
   }
-  valFunction(val: string) {
-    return 'asd';
+
+  copyMessageForPcsResolution() {
+    // Download value from local storage and parse it to float
+    const downloadValue = parseFloat(
+      localStorage.getItem('numberOfPxStorageValue')!
+    );
+
+    // Set middleValue for calculation
+    let middleValue = downloadValue / 1920;
+
+    // Calculate min and max value
+    let minValue = middleValue * 776;
+    let maxValue = middleValue * 1920;
+
+    // End calculation for middleValue
+    middleValue = middleValue * 100;
+
+    // Create CSS `clamp()` for calculations
+    this.clampForPcs = `clamp(min(${minValue.toFixed(
+      2
+    )}px), ${middleValue.toFixed(2)}vw, max(${maxValue.toFixed(2)}px));`;
+
+    console.log(this.clampForPcs);
+
+    // Return copied phrase for CSS
+    return this.clipboard.copy(this.clampForPcs);
   }
 }
